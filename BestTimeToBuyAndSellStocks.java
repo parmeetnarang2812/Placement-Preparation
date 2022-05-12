5d9bde973df6c9b2a36b9efaf61400ddea7809c2
Parmeet Kaur Narang <56155933+parmeetnarang2812@users.noreply.github.com>
2022-05-12 23:48:34 +0530

    -

---
+++ BestTimeToBuyAndSellStocks.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        // initially
+        int minPrice = prices[0];
+        int maxProfit = 0;
+        
+        // traverse the array
+        for(int price : prices) {
+            // calc. min price
+            if(price < minPrice){
+                minPrice = price;
+            }  
+            // calc. max profit
+            maxProfit = Math.max(maxProfit, price-minPrice);
+        }
+        return maxProfit;
+    }
+}                                       

44a15944253e06ba80baeb281a6211048ac3d947
Parmeet Kaur Narang <56155933+parmeetnarang2812@users.noreply.github.com>
2022-06-29 14:20:44 +0530

    Update 50-powx-n.java

--- 50-powx-n/50-powx-n.java
+++ 50-powx-n/50-powx-n.java
@@ -1,22 +1,27 @@
 class Solution {
+
     public double myPow(double x, int n) {
         double ans = 1.0;
         long nn = n;
-        if(nn<0) {
+        if (nn < 0) {
             nn = -1 * nn;
         }
-        while(nn>0) {
-            if(nn%2==0) {
-                x = x*x;
-                nn = nn/2;
-            }
-            else {
-                ans = ans * x;
-                nn = nn - 1;
-            }
+        // brute force -> O(n) -> TLE
+        for(int i=0; i<nn; i++) {
+            ans = ans * x;
         }
-        if(n<0) {
-            ans = (double)(1.0)/(double)(ans);
+        // optimized -> O(logn)
+        // while (nn > 0) {
+        //     if (nn % 2 == 0) {
+        //         x = x * x;
+        //         nn = nn / 2;
+        //     } else {
+        //         ans = ans * x;
+        //         nn = nn - 1;
+        //     }
+        // }
+        if (n < 0) {
+            ans = (double) (1.0) / (double) (ans);
         }
         return ans;
     }

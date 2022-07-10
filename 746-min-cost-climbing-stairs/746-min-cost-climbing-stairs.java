6268822ea7b5e4f670b551e79bc975ffe2ce3aaa
Parmeet Kaur Narang <56155933+parmeetnarang2812@users.noreply.github.com>
2022-07-10 15:21:55 +0530

    Time: 1 ms (89.45%), Space: 43.3 MB (74.23%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.java
@@ -1,20 +1,16 @@
 class Solution {
-
     public int minCostClimbingStairs(int[] cost) {
-        int n = cost.length;
-        int[] dp = new int[n + 1];
-        return climb_mem(n, cost, dp);
+        int n=cost.length;
+        return minCostClimbingStairs_optimiztion(n,cost);
     }
 
-    public int climb_mem(int N, int[] cost, int[] dp) {
-        for(int n=0; n<=N; n++) {
-            if (n <= 1) {
-            dp[n] = cost[n];
-            continue;
-        }
-
-        dp[n] = Math.min(dp[n-1], dp[n-2]) + (n == cost.length ? 0 : cost[n]);
-        }
-        return dp[N];
+    public int minCostClimbingStairs_optimiztion(int N, int[] cost) {
+        int one = 0, two = 0;
+        for (int i=0;i<N;i++){
+            int current = Math.min(one, two) + cost[i];
+            one = two;
+            two = current;
+        }  
+        return Math.min(one, two);
     }
 }

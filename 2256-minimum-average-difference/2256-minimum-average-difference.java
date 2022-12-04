12d518e1e723318a6b54517d310ecb28b4043150
Parmeet Kaur Narang <56155933+parmeetnarang2812@users.noreply.github.com>
2022-12-04 23:30:22 +0530

    Time: 20 ms (83.41%), Space: 74.3 MB (72.49%) - LeetHub

---
+++ 2256-minimum-average-difference/2256-minimum-average-difference.java
@@ -0,0 +1,24 @@
+class Solution {
+
+    public int minimumAverageDifference(int[] nums) {
+        long min = Integer.MAX_VALUE, sum = 0;
+        for (int i : nums) sum += i;
+        int leftIndex = 0, result = -1;
+        long prefixSum = 0;
+
+        while (leftIndex < nums.length) {
+            prefixSum += nums[leftIndex];
+            long leftAverage = prefixSum / (leftIndex + 1);
+            long sumRight = sum - prefixSum;
+            if (sumRight != 0) sumRight /= nums.length - leftIndex - 1;
+            long res = Math.abs(leftAverage - sumRight);
+            if (res < min) {
+                min = res;
+                result = leftIndex;
+            }
+            leftIndex++;
+        }
+        return result;
+    }
+    
+}

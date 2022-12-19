09dfba8103f86506025f584aaea750b10324e4e5
Parmeet Kaur Narang <56155933+parmeetnarang2812@users.noreply.github.com>
2022-12-19 18:23:07 +0530

    Time: 200 ms (35.42%), Space: 54.2 MB (95.29%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.java
@@ -0,0 +1,21 @@
+class Solution {
+
+    public int[] dailyTemperatures(int[] temperatures) {
+        Stack<Integer> st = new Stack<>();  
+        int[] ans = new int[temperatures.length];
+
+        for (int i = temperatures.length - 1; i >= 0; i--) {
+            while (!st.empty() && temperatures[st.peek()] <= temperatures[i]) { 
+                //if element(temperatures[idx]) at top is less than current element(temperatures[current_idx])
+                st.pop();   //then pop
+            }
+            if (!st.empty()) {
+                ans[i] = st.peek() - i; //storing difference of indices in array 
+            }
+            st.push(i); //pushing indices in stack
+        }
+
+        return ans;
+    }
+    
+}
